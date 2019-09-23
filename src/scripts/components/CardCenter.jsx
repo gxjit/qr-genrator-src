35b059c71b8ba9d78aa9b595dfd6138565c71f23
Gurjit Singh <g.s@outlook.in>
2019-09-23 17:34:50 +0530

    fixes

--- src/scripts/components/CardCenter.jsx
+++ src/scripts/components/CardCenter.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default ({ children }) => (
   <div className='row vh-100-fix d-flex flex-column justify-content-center align-items-center'>
     <div className='col-12 col-sm-10 col-md-8 col-lg-6'>
-      <div className='card h-100 bg-primary rounded-lg shadow-lg'>
+      <div className='card h-100 bg-primary rounded-lg shadow-lg h-85'>
         <div className='card-body d-flex flex-column justify-content-center align-items-center bg-primary'>
           {children}
         </div>
