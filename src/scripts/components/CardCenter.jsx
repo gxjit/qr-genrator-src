e80cb756610215c699a9c8941be47aac1ece45ff
Gurjit Singh <g.s@outlook.in>
2019-09-24 13:54:28 +0530

    Added License

--- src/scripts/components/CardCenter.jsx
+++ src/scripts/components/CardCenter.jsx
@@ -4,7 +4,7 @@ export default ({ children }) => (
   <div className='row vh-100-fix d-flex flex-column justify-content-center align-items-center'>
     <div className='col-12 col-sm-10 col-md-8 col-lg-6'>
       <div className='card h-100 bg-primary rounded-lg shadow-lg h-85'>
-        <div className='card-body d-flex flex-column justify-content-center align-items-center bg-primary'>
+        <div className='card-body d-flex flex-column justify-content-center align-items-center bg-primary h-100'>
           {children}
         </div>
       </div>
